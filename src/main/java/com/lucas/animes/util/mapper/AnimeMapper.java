62ba20a380b5861f11aca2eb31f662f15f4c506b
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-01-25 20:24:18 -0300

    feat(Mapper): Mapscruct
    Adding mapscruct depedency to map the dto's.

--- src/main/java/com/lucas/animes/util/mapper/AnimeMapper.java
+++ src/main/java/com/lucas/animes/util/mapper/AnimeMapper.java
@@ -1,4 +1,14 @@
 package com.lucas.animes.util.mapper;
 
-public class AnimeMapper {
+import com.lucas.animes.api.request.AnimeRequestBody;
+import com.lucas.animes.entity.Anime;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface AnimeMapper {
+
+	AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);
+
+	Anime toAnime(AnimeRequestBody animeRequestBody);
 }
