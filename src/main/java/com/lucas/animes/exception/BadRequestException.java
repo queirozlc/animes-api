f84c50bd4945a69f6c1b18a202867dbabe17fa96
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-01-25 21:05:38 -0300

    refactor: Exception.
    Added a global exception handling for bad requests.

--- src/main/java/com/lucas/animes/exception/BadRequestException.java
+++ src/main/java/com/lucas/animes/exception/BadRequestException.java
@@ -1,16 +1,19 @@
 package com.lucas.animes.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException{
 
 	public BadRequestException(String message) {
 		super(message);
 	}
 
-	static public <T> T requireNonNull(T obj, String message) {
+	static public <T> void requireNonNull(T obj, String message) {
 		if (obj == null) {
 			throw new BadRequestException(message);
 		}
-		return obj;
 	}
 
 }
