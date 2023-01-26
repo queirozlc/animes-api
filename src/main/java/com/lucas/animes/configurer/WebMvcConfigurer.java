db92a2ccaf343c7ebfd13753043dc175d9f7c572
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-01-25 22:20:43 -0300

    feat: Pagination
    Endpoint of findAll animes turned to pageable instead list. Overriding spring default configuration.

---
+++ src/main/java/com/lucas/animes/configurer/WebMvcConfigurer.java
@@ -0,0 +1,20 @@
+package com.lucas.animes.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfigurer
+		implements org.springframework.web.servlet.config.annotation.WebMvcConfigurer {
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		PageableHandlerMethodArgumentResolver pageHandler = new PageableHandlerMethodArgumentResolver();
+		pageHandler.setFallbackPageable(PageRequest.of(0, 5));
+		resolvers.add(pageHandler);
+	}
+}
